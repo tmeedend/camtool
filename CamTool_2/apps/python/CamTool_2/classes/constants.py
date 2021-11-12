12a0408705dc92eb05ffb1f51d9dc35385421b9e
Théo MEEDENDORP <tmeedend@gmail.com>
2021-11-12 10:19:42 +0100

    add constants module

---
+++ CamTool_2/apps/python/CamTool_2/classes/constants.py
@@ -0,0 +1,23 @@
+
+import os
+
+G_FONT = "Arial"
+G_IMG_ROOT = "apps/python/CamTool_2/img/"
+
+
+G_IMG_CAMERA = G_IMG_ROOT + "camera.png"
+G_IMG_KEYFRAME = G_IMG_ROOT + "keyframe.png"
+G_IMG_NEXT = G_IMG_ROOT + "next.png"
+G_IMG_NEXT_DISABLED = G_IMG_ROOT + "next_disabled.png"
+G_IMG_NEXT_PLUS = G_IMG_ROOT + "next+.png"
+G_IMG_OFF = G_IMG_ROOT + "off.png"
+G_IMG_ON = G_IMG_ROOT + "on.png"
+G_IMG_POSITION = G_IMG_ROOT + "pos.png"
+G_IMG_POSITION_ACTIVE = G_IMG_ROOT + "pos_active.png"
+G_IMG_PREVIOUS = G_IMG_ROOT + "prev.png"
+G_IMG_PREVIOUS_DISABLED = G_IMG_ROOT + "prev_disabled.png"
+G_IMG_PREVIOUS_PLUS = G_IMG_ROOT + "prev+.png"
+G_IMG_REMOVE = G_IMG_ROOT + "remove.png"
+G_IMG_RESET = G_IMG_ROOT + "reset.png"
+G_IMG_TIME = G_IMG_ROOT + "time.png"
+G_IMG_TIME_ACTIVE = G_IMG_ROOT + "time_active.png"
